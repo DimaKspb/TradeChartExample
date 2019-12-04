a0bfc6d6f037f1b961213f9a20d3dda46777e27e
dima <dimakrash@gmail.com>
2019-12-05 00:18:42 +0300

    Revert "refact"
    This reverts commit 67dadd45

--- app/src/main/java/com/dima/tradechart/MainActivity.kt
+++ app/src/main/java/com/dima/tradechart/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.dima.tradechart
 
-import androidx.appcompat.app.AppCompatActivity
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.dima.tradechart.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
